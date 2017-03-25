6c69a935c993dd8751dea37bcbf0a5852e200a5a
Fan <fanfeng2015@gmail.com>
2017-03-24 22:49:18 -0400

    #26

--- ArrayDeduplication/src/arrayDeduplication/ArrayDeduplication.java
+++ ArrayDeduplication/src/arrayDeduplication/ArrayDeduplication.java
@@ -2,6 +2,8 @@ package arrayDeduplication;
 
 import java.util.Arrays;
 
+// LeetCode #26
+
 // Given a sorted integer array, remove duplicate elements. 
 // For each group of elements with the same value keep only one of them. 
 // Do this in-place, using the left side of the original array and maintain
@@ -25,5 +27,4 @@ public class ArrayDeduplication {
 
 	// Time complexity is O(n).
 	// Space complexity is O(1).
-
 }
