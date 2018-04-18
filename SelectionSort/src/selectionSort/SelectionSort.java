39129a1a5defde416c4850366fe7014d865bbda1
Fan Feng <fanfeng2015@gmail.com>
2018-04-17 23:42:14 -0700

    LeetCode N/A

--- SelectionSort/src/selectionSort/SelectionSort.java
+++ SelectionSort/src/selectionSort/SelectionSort.java
@@ -32,22 +32,4 @@ public class SelectionSort {
 	
 	// Time complexity is O(n^2) in best case, average case, and worst case.
 	// Space complexity is O(1).
-
-	public static void main(String[] args) {
-		SelectionSort selectionSort = new SelectionSort();
-		int[] array = null;
-		System.out.println(Arrays.toString(selectionSort.sort(array)));
-		array = new int[0];
-		System.out.println(Arrays.toString(selectionSort.sort(array)));
-		array = new int[] {0};
-		System.out.println(Arrays.toString(selectionSort.sort(array)));
-		array = new int[] {-1, -3, 4, 7};
-		System.out.println(Arrays.toString(selectionSort.sort(array)));
-		array = new int[] {1, 2, 3, 4};
-		System.out.println(Arrays.toString(selectionSort.sort(array)));
-		array = new int[] {4, 3, 2, 1};
-		System.out.println(Arrays.toString(selectionSort.sort(array)));
-		array = new int[] {2, 4, 1, 5, 3};
-		System.out.println(Arrays.toString(selectionSort.sort(array)));
-	}
 }
