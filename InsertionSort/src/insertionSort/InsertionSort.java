5a54af053036e9b744937434f97efc9ab69099a0
Fan Feng <fanfeng2015@gmail.com>
2018-04-18 22:31:43 -0700

    LeetCode N/A

--- InsertionSort/src/insertionSort/InsertionSort.java
+++ InsertionSort/src/insertionSort/InsertionSort.java
@@ -1,7 +1,5 @@
 package insertionSort;
 
-import java.util.Arrays;
-
 public class InsertionSort {
 
 	public int[] sort(int[] array) {
@@ -27,23 +25,5 @@ public class InsertionSort {
 
 	// Time complexity is O(n) in best case, O(n^2) in average case and worst case.
 	// Space complexity is O(1).
-
-	public static void main(String[] args) {
-		InsertionSort selectionSort = new InsertionSort();
-		int[] array = null;
-		System.out.println(Arrays.toString(selectionSort.sort(array)));
-		array = new int[0];
-		System.out.println(Arrays.toString(selectionSort.sort(array)));
-		array = new int[] { 0 };
-		System.out.println(Arrays.toString(selectionSort.sort(array)));
-		array = new int[] { -1, -3, 4, 7 };
-		System.out.println(Arrays.toString(selectionSort.sort(array)));
-		array = new int[] { 1, 2, 3, 4 };
-		System.out.println(Arrays.toString(selectionSort.sort(array)));
-		array = new int[] { 4, 3, 2, 1 };
-		System.out.println(Arrays.toString(selectionSort.sort(array)));
-		array = new int[] { 2, 4, 1, 5, 3 };
-		System.out.println(Arrays.toString(selectionSort.sort(array)));
-	}
-
 }
+
