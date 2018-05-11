3139d503296d8afc86fff00c077f4a5c03dffc80
Fan Feng <fanfeng2015@gmail.com>
2018-05-10 19:41:59 -0700

    LeetCode N/A

--- GetKeysInGivenRangeInBST/src/getKeysInGivenRangeInBST/GetKeysInGivenRangeInBST.java
+++ GetKeysInGivenRangeInBST/src/getKeysInGivenRangeInBST/GetKeysInGivenRangeInBST.java
@@ -1,13 +1,16 @@
 package getKeysInGivenRangeInBST;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
-// Get the list of keys in a given binary search tree in a given range[min, max] 
+// Get the list of keys in a given binary search tree in a given range [min, max] 
 // in ascending order, both min and max are inclusive.
 
 public class GetKeysInGivenRangeInBST {
 
-	public List<Integer> getRange(TreeNode root, int min, int max) {
+	// recursive solution
+	public List<Integer> getRange1(TreeNode root, int min, int max) {
 		List<Integer> result = new ArrayList<>();
 		helper(root, min, max, result);
 		return result;
@@ -30,4 +33,32 @@ public class GetKeysInGivenRangeInBST {
 
 	// Time complexity is O(n).
 	// Space complexity is O(n), when the BST is highly unbalanced.
+	
+	// iterative solution: in-order traversal of a BST is monotonically increasing
+	public List<Integer> getRange2(TreeNode root, int min, int max) {
+	    List<Integer> result = new ArrayList<>();
+	    LinkedList<TreeNode> stack = new LinkedList<>();
+	    TreeNode cur = root;
+	    while (cur != null || !stack.isEmpty()) {
+	    	if (cur != null) {
+	    		stack.offerFirst(cur);
+	    		cur = cur.left;
+	    	}
+	    	else {
+	    		cur = stack.pollFirst();
+	    		if (cur.key > max) {
+	    			break;
+	    		}
+	    		if (cur.key >= min && cur.key <= max) {
+	    			result.add(cur.key);
+	    		}
+	    		cur = cur.right;
+	    	}
+	    }
+	    return result;
+	}
+	
+	// Time complexity is O(n).
+	// Space complexity is O(n), but since object is stored on heap, iterative solution 
+	// avoids stack overflow.
 }
