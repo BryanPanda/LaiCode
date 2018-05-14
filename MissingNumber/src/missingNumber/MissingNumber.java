057c2166392df2c0b4e8f1e1d1c3dcb9104f813e
Fan Feng <fanfeng2015@gmail.com>
2018-05-13 22:31:41 -0700

    LeetCode #268

--- MissingNumber/src/missingNumber/MissingNumber.java
+++ MissingNumber/src/missingNumber/MissingNumber.java
@@ -3,6 +3,8 @@ package missingNumber;
 import java.util.HashSet;
 import java.util.Set;
 
+// LeetCode #268 (Missing Number).
+
 // Given an integer array of size N - 1, containing all the numbers from 1 to N except one, find the missing number.
 
 // Assumption: The given array is not null, and N >= 1
@@ -14,15 +16,14 @@ public class MissingNumber {
 	// the difference of the two is the missing number
 	// potential problem: overflow
 	public int missing1(int[] array) {
-		long expectation = 0L;
 		long result = 0L;
 		for (int i = 1; i <= array.length + 1; i++) {
-			expectation += i;
+			result += i;
 		}
 		for (int i = 0; i < array.length; i++) {
-			result += array[i];
+			result -= array[i];
 		}
-		return (int) (expectation - result);
+		return (int) (result);
 	}
 
 	// Time Complexity is O(n).
@@ -30,12 +31,11 @@ public class MissingNumber {
 
 	// solution 2: exclusive-or
 	public int missing2(int[] array) {
-		// Write your solution here.
 		int result = 0;
 		for (int i = 1; i <= array.length + 1; i++) {
 			result = result ^ i;
 		}
-		for (int i = 0; i <= array.length; i++) {
+		for (int i = 0; i < array.length; i++) {
 			result = result ^ array[i];
 		}
 		return result;
@@ -47,12 +47,11 @@ public class MissingNumber {
 	// solution 3: hash set
 	public int missing3(int[] array) {
 		int result = 0;
-		int n = array.length + 1;
 		Set<Integer> set = new HashSet<Integer>();
 		for (int i = 0; i < array.length; i++) {
 			set.add(array[i]);
 		}
-		for (int i = 1; i <= n; i++) {
+		for (int i = 1; i <= array.length + 1; i++) {
 			if (!set.contains(i)) {
 				result = i;
 				break;
@@ -64,7 +63,7 @@ public class MissingNumber {
 	// Time Complexity is O(n).
 	// Space Complexity is O(n).
 
-	// solution 4
+	// solution 4: swap
 	public int missing4(int[] array) {
 		for (int i = 0; i < array.length; i++) {
 			// number (i + 1) should be at position i, except for n,
