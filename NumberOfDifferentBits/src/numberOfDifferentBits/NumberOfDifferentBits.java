f8882a9c807bfd8932bc728cf6078bad058f95a1
Fan Feng <fanfeng2015@gmail.com>
2018-05-18 16:27:51 -0700

    LeetCode N/A

--- NumberOfDifferentBits/src/numberOfDifferentBits/NumberOfDifferentBits.java
+++ NumberOfDifferentBits/src/numberOfDifferentBits/NumberOfDifferentBits.java
@@ -5,12 +5,13 @@ package numberOfDifferentBits;
 public class NumberOfDifferentBits {
 
 	public int diffBits(int a, int b) {
-		a ^= b;
+		int c = a ^ b;
 		int count = 0;
-		while (a != 0) {
-			count += a & 1;
-			a >>>= 1;
+		while (c != 0) {
+			count += c & 1;
+			c >>>= 1;
 		}
 		return count;
 	}
+	
 }
