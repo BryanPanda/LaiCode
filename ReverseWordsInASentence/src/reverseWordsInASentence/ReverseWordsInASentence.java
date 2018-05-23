fb439abe1e9e96d59e2daca18cd4cbf08b163784
Fan Feng <fanfeng2015@gmail.com>
2018-05-22 22:09:46 -0700

    LeetCode #186

--- ReverseWordsInASentence/src/reverseWordsInASentence/ReverseWordsInASentence.java
+++ ReverseWordsInASentence/src/reverseWordsInASentence/ReverseWordsInASentence.java
@@ -1,5 +1,7 @@
 package reverseWordsInASentence;
 
+// LeetCode #186 (Reverse Words in a String II).
+
 // Reverse the words in a sentence.
 
 // Assumptions:
@@ -9,52 +11,33 @@ package reverseWordsInASentence;
 public class ReverseWordsInASentence {
 
 	public String reverseWords(String input) {
-		char[] array = input.toCharArray();
-		reverse(array, 0, array.length - 1);
-		int index = 0;
-		while (index < array.length) {
-			int start = index;
-			while (index < array.length - 1 && array[index + 1] != ' ') {
-				index++;
-			}
-			reverse(array, start, index);
-			index += 2;
+		if (input == null) {
+			return input;
 		}
-		return new String(array);
+		char[] array = input.toCharArray();
+        int slow = 0;
+        for (int fast = 0; fast < array.length; fast++) {
+            if (array[fast] != ' ' && (fast == 0 || array[fast - 1] == ' ')) {
+                slow = fast;
+            }
+            if (array[fast] != ' ' && (fast == array.length - 1 || array[fast + 1] == ' ')) {
+                reverse(array, slow, fast);
+            }
+        }
+        reverse(array, 0, array.length - 1);
+        return new String(array);
 	}
+    
+    private void reverse(char[] array, int left, int right) {
+        while (left < right) {
+            char temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++;
+            right--;
+        }
+    }
 
 	// Time complexity is O(n).
 	// Space complexity is O(n).
-
-	// another solution: use for loop to control iterations
-	public String reverseWords2(String input) {
-		char[] array = input.toCharArray();
-		reverse(array, 0, array.length - 1);
-		int start = 0;
-		for (int i = 0; i < array.length; i++) {
-			if (array[i] != ' ' && (i == 0 || array[i - 1] == ' ')) {
-				start = i;
-			}
-			if (array[i] != ' ' && (i == array.length - 1 || array[i + 1] == ' ')) {
-				reverse(array, start, i);
-			}
-		}
-		return new String(array);
-	}
-
-	// Time complexity is O(n).
-	// Space complexity is O(n).
-
-	private void reverse(char[] array, int left, int right) {
-		if (left >= right) {
-			return;
-		}
-		while (left < right) {
-			char temp = array[left];
-			array[left] = array[right];
-			array[right] = temp;
-			left++;
-			right--;
-		}
-	}
 }
