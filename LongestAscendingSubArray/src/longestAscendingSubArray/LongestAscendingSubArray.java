46e428dda899fbe238dc5d21de08a98cef638ac3
Fan Feng <fanfeng2015@gmail.com>
2018-05-22 23:55:59 -0700

    LeetCode #674

--- LongestAscendingSubArray/src/longestAscendingSubArray/LongestAscendingSubArray.java
+++ LongestAscendingSubArray/src/longestAscendingSubArray/LongestAscendingSubArray.java
@@ -1,5 +1,7 @@
 package longestAscendingSubArray;
 
+// LeetCode #674 (Longest Continuous Increasing Subsequence).
+
 // Given an unsorted array, find the length of the longest subarray in which the numbers are in ascending order.
 
 // Assumption: The given array is not null
@@ -11,16 +13,16 @@ public class LongestAscendingSubArray {
 	// M[i] = M[i - 1] + 1, if array[i] > array[i - 1]
 	// M[i] = 1, otherwise
 	public int longest(int[] array) { // optimized solution
-		if (array.length == 0) {
+		if (array == null || array.length == 0) {
 			return 0;
 		}
 		int max = 1, cur = 1;
-		for (int i = 0; i < array.length; i++) {
-			if (i == 0 || array[i] <= array[i - 1]) {
-				cur = 1;
-			} else {
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] > array[i - 1]) {
 				cur++;
 				max = Math.max(max, cur);
+			} else {
+				cur = 1;
 			}
 		}
 		return max;
