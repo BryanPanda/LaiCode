ac5e898e343ecee3ee0d952fb2e8014b28c569bb
Fan Feng <fanfeng2015@gmail.com>
2018-05-25 16:44:59 -0700

    LeetCode n/a

--- ArrayHopper4/src/arrayHopper4/ArrayHopper4.java
+++ ArrayHopper4/src/arrayHopper4/ArrayHopper4.java
@@ -1,6 +1,9 @@
 package arrayHopper4;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
 
 // Given an array A of non-negative integers, you are initially positioned at an arbitrary index of the array. 
 // A[i] means the maximum jump distance from that position (you can either jump left or jump right). 
@@ -17,42 +20,29 @@ public class ArrayHopper4 {
 		if (array.length == 1) {
 			return 0;
 		}
-		int[] minJump = forward(array);
-		minJump = bothDirection(array, minJump);
-		return minJump[index] == Integer.MAX_VALUE ? -1 : minJump[index];
-	}
-
-	private int[] forward(int[] array) {
-		// guaranteed array.length > 1
+		Set<Integer> set = new HashSet<>();
+		LinkedList<Integer> queue = new LinkedList<>();
+		
 		int[] minJump = new int[array.length];
 		Arrays.fill(minJump, Integer.MAX_VALUE);
-		minJump[minJump.length - 1] = 0;
-		for (int i = array.length - 2; i >= 0; i--) {
-			if (i + array[i] >= array.length - 1) {
-				minJump[i] = 1;
-			} else {
-				for (int j = 1; j <= array[i]; j++) {
-					if (minJump[i + j] < Integer.MAX_VALUE) {
-						minJump[i] = Math.min(minJump[i], minJump[i + j] + 1);
+		minJump[index] = 0;
+		
+		set.add(index);
+		queue.offerFirst(index);
+		while (!queue.isEmpty()) {
+			Integer cur = queue.pollLast();
+			for (int i = cur - array[cur]; i <= cur + array[cur]; i++) {
+				if (i >= 0 && i <= array.length - 1) {
+					minJump[i] = Math.min(minJump[i], minJump[cur] + 1);
+					if (set.add(i)) {
+						queue.offerFirst(i);
 					}
 				}
 			}
 		}
-		return minJump;
+		
+		return minJump[minJump.length - 1] == Integer.MAX_VALUE ? -1 : minJump[minJump.length - 1];
 	}
 
-	private int[] bothDirection(int[] array, int[] minJump) {
-		for (int i = array.length - 1; i >= 0; i--) {
-			for (int j = 1; j <= array[i] && i - j >= 0 && minJump[i - j] < Integer.MAX_VALUE; j++) {
-				minJump[i] = Math.min(minJump[i], 1 + minJump[i - j]);
-			}
-		}
-		return minJump;
-	}
-
-	public static void main(String[] args) {
-		ArrayHopper4 arrayHopper4 = new ArrayHopper4();
-		System.out.println(arrayHopper4.minJump(new int[] { 5, 1, 1, 1, 0, 4 }, 2));
-	}
 
 }
