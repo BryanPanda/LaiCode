968fc5fff675320a7d7123891a8fcf6144119e14
Fan Feng <fanfeng2015@gmail.com>
2018-05-25 15:21:39 -0700

    LeetCode N/A

--- ArrayHopper3/src/arrayHopper3/ArrayHopper3.java
+++ ArrayHopper3/src/arrayHopper3/ArrayHopper3.java
@@ -12,65 +12,32 @@ import java.util.Arrays;
 
 public class ArrayHopper3 {
 
-	// DP solution 1
+	// DP
+	// M[i]: from index 0, minimum number of jumps to index i
 	public int minJump(int[] array) {
-		if (array.length == 1) {
-			return array[0] == 0 ? -1 : 1;
-		}
-		int[] minJump = new int[array.length];
-		Arrays.fill(minJump, Integer.MAX_VALUE);
-		minJump[minJump.length - 1] = array[array.length - 1] == 0 ? Integer.MAX_VALUE : 1;
-		for (int i = array.length - 2; i >= 0; i--) {
-			if (i + array[i] > array.length - 1) {
-				minJump[i] = 1;
-			} else {
-				for (int j = 1; j <= array[i]; j++) {
-					if (minJump[i + j] < Integer.MAX_VALUE) {
-						minJump[i] = Math.min(minJump[i], minJump[i + j] + 1);
-					}
-				}
-			}
-		}
-		return minJump[0] == Integer.MAX_VALUE ? -1 : minJump[0];
-	}
-
-	// Time complexity is O(n^2).
-	// Space complexity is O(n).
-
-	// DP solution 2
-	public int minJump2(int[] array) {
-		if (array.length == 1) {
-			return array[0] == 0 ? -1 : 1;
-		}
 		int[] minJump = new int[array.length + 1];
-		for (int i = 1; i < array.length + 1; i++) {
-			minJump[i] = -1;
-			for (int j = i - 1; j >= 0; j--) {
-				if (j + array[j] >= i && minJump[j] != -1) {
-					if (minJump[i] == -1 || minJump[i] > minJump[j] + 1) {
-						minJump[i] = minJump[j] + 1;
-					}
+		Arrays.fill(minJump, Integer.MAX_VALUE);
+		minJump[0] = 0;
+		for (int i = 1; i <= array.length; i++) {
+			for (int j = 0; j < i; j++) {
+				if (minJump[j] != Integer.MAX_VALUE && j + array[j] >= i) {
+					minJump[i] = Math.min(minJump[i], minJump[j] + 1);
 				}
 			}
 		}
-		return minJump[minJump.length - 1];
+		return minJump[minJump.length - 1] == Integer.MAX_VALUE ? -1 : minJump[minJump.length - 1];
 	}
 
 	// Time Complexity is O(n^2).
 	// Space Complexity is O(n).
 
-	// solution 3: greedy
-	public int minJump3(int[] array) {
-		if (array.length == 1) {
-			return array[0] == 0 ? -1 : 1;
-		}
+	// greedy
+	public int minJump2(int[] array) {
 		int jump = 0; // number of jumps so far
-		int cur = 0; // max index that current steps can reach
-		int next = 0; // max index that (current + 1) steps can reach
+		int cur = 0; // max index that jump steps can reach
+		int next = 0; // max index that (jump + 1) steps can reach
 		for (int i = 0; i <= array.length; i++) {
-			if (i > cur) {
-				// if index i can NOT be reached by current steps
-				// jump one more step
+			if (i > cur) { // index i can not be reached by jump steps
 				jump++;
 				if (cur == next) {
 					return -1;
