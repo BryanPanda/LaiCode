d39c344eb8037f954415ff5a3856424850f17e07
Fan Feng <fanfeng2015@gmail.com>
2018-05-26 16:29:31 -0700

    LeetCode #188

--- BuyStock4/src/buyStock4/BuyStock4.java
+++ BuyStock4/src/buyStock4/BuyStock4.java
@@ -2,6 +2,8 @@ package buyStock4;
 
 import java.util.Arrays;
 
+//LeetCode #188 (Best Time to Buy and Sell Stock IV).
+
 // Given an array of integers representing a stock’s price on each day. 
 // On each day you can only make one operation: either buy or sell one unit of stock, 
 // and at any time you can only hold at most one unit of stock, and you can make at most K transactions in total. 
@@ -11,23 +13,19 @@ import java.util.Arrays;
 
 public class BuyStock4 {
 
-	// generalized solution from BuyStock3
+	// generalize solution from BuyStock3
 	public int maxProfit(int[] array, int k) {
-		if (k <= 0) {
-			return 0;
-		}
+        k = Math.min(k, array.length / 2); // optimization
+        if (k == 0) {
+            return 0;
+        }
 		int[] buy = new int[k];
 		Arrays.fill(buy, Integer.MIN_VALUE);
 		int[] sell = new int[k];
 		for (int i = 0; i < array.length; i++) {
 			for (int j = 0; j < k; j++) {
-				if (j == 0) {
-					buy[j] = Math.max(buy[j], -array[i]);
-					sell[j] = Math.max(sell[j], buy[j] + array[i]);
-				} else {
-					buy[j] = Math.max(buy[j], sell[j - 1] - array[i]);
-					sell[j] = Math.max(sell[j], buy[j] + array[i]);
-				}
+				buy[j] = Math.max(buy[j], (j == 0 ? 0 : sell[j - 1]) - array[i]);
+				sell[j] = Math.max(sell[j], buy[j] + array[i]);
 			}
 		}
 		return sell[k - 1];
