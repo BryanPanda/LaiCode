0a76ab9ca30ddc3b28bb0f29d7350971a748f11b
Fan Feng <fanfeng2015@gmail.com>
2018-05-28 14:09:31 -0700

    LeetCode #485

--- LongestConsecutiveOnes/src/longestConsecutiveOnes/LongestConsecutiveOnes.java
+++ LongestConsecutiveOnes/src/longestConsecutiveOnes/LongestConsecutiveOnes.java
@@ -12,7 +12,7 @@ public class LongestConsecutiveOnes {
 		if (array.length == 0) {
 			return 0;
 		}
-		int cur = 0, max = 0;
+		int cur = 0, max = 1;
 		for (int i = 0; i < array.length; i++) {
 			cur = array[i] == 0 ? 0 : cur + 1;
 			max = Math.max(cur, max);
