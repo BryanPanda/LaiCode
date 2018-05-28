7363567b1340882bf8219aad9677b6eb84eba22a
Fan Feng <fanfeng2015@gmail.com>
2018-05-28 16:47:44 -0700

    LeetCode #295

--- MedianTracker/src/medianTracker/MedianTracker.java
+++ MedianTracker/src/medianTracker/MedianTracker.java
@@ -3,6 +3,8 @@ package medianTracker;
 import java.util.Collections;
 import java.util.PriorityQueue;
 
+// LeetCode #295 (Find Median from Data Stream).
+
 // Given an unlimited flow of numbers, keep track of the median of all elements seen so far.
 // You will have to implement the following two methods for the class
 // 1. read(int value): read one value from the flow 
@@ -10,12 +12,12 @@ import java.util.PriorityQueue;
 
 public class MedianTracker {
 
-	private PriorityQueue<Integer> largerHalf; // min heap
 	private PriorityQueue<Integer> smallerHalf; // max heap
+	private PriorityQueue<Integer> largerHalf; // min heap
 
 	public MedianTracker() {
-		this.largerHalf = new PriorityQueue<>();
 		this.smallerHalf = new PriorityQueue<>(11, Collections.reverseOrder());
+		this.largerHalf = new PriorityQueue<>();
 	}
 
 	// maintain smallerHalf.size() == largerHalf.size(), when total number of
@@ -33,6 +35,8 @@ public class MedianTracker {
 			smallerHalf.offer(largerHalf.poll());
 		}
 	}
+	
+	// Time complexity is O(n).
 
 	public Double median() {
 		int n = smallerHalf.size() + largerHalf.size();
@@ -44,4 +48,8 @@ public class MedianTracker {
 			return (smallerHalf.peek() + largerHalf.peek()) / 2.0;
 		}
 	}
+	
+	// Time complexity is O(1).
+	
+	// Space complexity is O(n).
 }
