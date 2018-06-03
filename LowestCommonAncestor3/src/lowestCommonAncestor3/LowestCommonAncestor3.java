707fe423436bfc555efcdc5436520bbd9a56c17d
Fan Feng <fanfeng2015@gmail.com>
2018-06-02 20:46:19 -0700

    LeetCode #236

--- LowestCommonAncestor3/src/lowestCommonAncestor3/LowestCommonAncestor3.java
+++ LowestCommonAncestor3/src/lowestCommonAncestor3/LowestCommonAncestor3.java
@@ -1,5 +1,7 @@
 package lowestCommonAncestor3;
 
+// LeetCode #236 (Lowest Common Ancestor of a Binary Tree).
+
 // Given two nodes in a binary tree, find their lowest common ancestor.
 // Return null If any of the nodes is not in the tree.
 
@@ -20,21 +22,13 @@ public class LowestCommonAncestor3 {
 	}
 
 	private boolean find(TreeNode root, TreeNode node) {
-		// base case
 		if (root == null || node == null) {
 			return false;
 		}
 		if (root == node) {
 			return true;
 		}
-		// recursive rule
-		if (find(root.left, node)) {
-			return true;
-		}
-		if (find(root.right, node)) {
-			return true;
-		}
-		return false;
+		return find(root.left, node) || find(root.right, node);
 	}
 
 	public TreeNode LCA(TreeNode root, TreeNode one, TreeNode two) {
@@ -52,16 +46,4 @@ public class LowestCommonAncestor3 {
 	// Time complexity is O(n).
 	// Space complexity is O(n) because of call-stack, if the binary tree is
 	// highly unbalanced.
-
-	public static void main(String[] args) {
-		LowestCommonAncestor3 lowestCommonAncestor3 = new LowestCommonAncestor3();
-		TreeNode root = new TreeNode(5);
-		root.left = new TreeNode(9);
-		root.right = new TreeNode(12);
-		root.left.left = new TreeNode(2);
-		root.left.right = new TreeNode(3);
-		root.right.right = new TreeNode(14);
-		System.out.println(lowestCommonAncestor3.lowestCommonAncestor(root, root.left.left, root.right.right).key);
-	}
-
 }
