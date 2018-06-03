4c8e4eab52de8a52bacf701e91d3ea7a45387261
Fan Feng <fanfeng2015@gmail.com>
2018-06-02 20:18:51 -0700

    LeetCode N/A

--- LargestAndSmallest/src/largestAndSmallest/LargestAndSmallest.java
+++ LargestAndSmallest/src/largestAndSmallest/LargestAndSmallest.java
@@ -3,11 +3,11 @@ package largestAndSmallest;
 // Use the least number of comparisons to get the largest and smallest number in the given integer array. 
 // Return the largest number and the smallest number.
 
-// Assumption: The given array is not null and has length of at least 1
+// Assumption: The given array is not null and has length of at least 1.
 
 public class LargestAndSmallest {
 
-	// binary reduction
+	// Binary reduction
 	public int[] largestAndSmallest(int[] array) {
 		for (int i = 0; i < array.length / 2; i++) {
 			if (array[i] > array[array.length - 1 - i]) {
@@ -16,8 +16,7 @@ public class LargestAndSmallest {
 		}
 		int min = Integer.MAX_VALUE;
 		int max = Integer.MIN_VALUE;
-		for (int i = 0; i <= (array.length - 1) / 2; i++) {
-			// why (array.length - 1) / 2?
+		for (int i = 0; i <= (array.length - 1) / 2; i++) { // why (array.length - 1) / 2?
 			if (array[i] < min) {
 				min = array[i];
 			}
@@ -30,14 +29,13 @@ public class LargestAndSmallest {
 		return new int[] { max, min };
 	}
 
-	// The number of comparison is 1.5 * N
-	// Time complexity is O(n).
-	// Space complexity is O(1).
-
 	private void swap(int[] array, int i, int j) {
 		int temp = array[i];
 		array[i] = array[j];
 		array[j] = temp;
 	}
-
+	
+	// The number of comparison is 1.5 * N
+	// Time complexity is O(n).
+	// Space complexity is O(1).
 }
