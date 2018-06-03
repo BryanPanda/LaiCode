d460339ebd776465291d82f6c192b3b94a9e4e31
Fan Feng <fanfeng2015@gmail.com>
2018-06-02 19:51:36 -0700

    LeetCode #48

--- RotateMatrix/src/rotateMatrix/RotateMatrix.java
+++ RotateMatrix/src/rotateMatrix/RotateMatrix.java
@@ -1,42 +1,16 @@
 package rotateMatrix;
 
+// LeetCode #48 (Rotate Image).
+
 // Rotate an N * N matrix clockwise 90 degrees.
 
 // Assumption: The matrix is not null and N >= 0.
 
 public class RotateMatrix {
-
-	// solution 1
+	
+	// First flip against x = 0, then flip against y = x.
 	public void rotate(int[][] matrix) {
 		int n = matrix.length;
-		if (n <= 1) {
-			return;
-		}
-		int round = n / 2;
-		for (int level = 0; level < round; level++) {
-			int left = level;
-			int right = n - 2 - level;
-			for (int i = left; i <= right; i++) {
-				int temp = matrix[left][i];
-				matrix[left][i] = matrix[n - 1 - i][left];
-				matrix[n - 1 - i][left] = matrix[n - 1 - left][n - 1 - i];
-				matrix[n - 1 - left][n - 1 - i] = matrix[i][n - 1 - left];
-				matrix[i][n - 1 - left] = temp;
-			}
-		}
-	}
-
-	// Time complexity is O(n^2).
-	// Space complexity is O(n^2).
-
-	// solution 2:
-	// rotate 90 degrees clock-wise = flip against y-axis, then flip against
-	// line y = x
-	public void rotate2(int[][] matrix) {
-		int n = matrix.length;
-		if (n <= 1) {
-			return;
-		}
 		flipAgainstY(matrix, n);
 		flipAgainstYEqualsX(matrix, n);
 	}
@@ -51,16 +25,16 @@ public class RotateMatrix {
 
 	private void flipAgainstYEqualsX(int[][] matrix, int n) {
 		for (int i = 0; i < n; i++) {
-			for (int j = 0; j + i < n - 1; j++) {
+			for (int j = 0; i + j < n - 1; j++) {
 				swap(matrix, i, j, n - 1 - j, n - 1 - i);
 			}
 		}
 	}
 
-	private void swap(int[][] matrix, int iRow, int iCol, int jRow, int jCol) {
-		int temp = matrix[iRow][iCol];
-		matrix[iRow][iCol] = matrix[jRow][jCol];
-		matrix[jRow][jCol] = temp;
+	private void swap(int[][] matrix, int aRow, int aCol, int bRow, int bCol) {
+		int temp = matrix[aRow][aCol];
+		matrix[aRow][aCol] = matrix[bRow][bCol];
+		matrix[bRow][bCol] = temp;
 	}
 
 	// Time complexity is O(n^2).
