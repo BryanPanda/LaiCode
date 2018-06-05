53897a8b34e13be96c1e008590accb8f26d0693d
Fan Feng <fanfeng2015@gmail.com>
2018-06-04 21:52:46 -0700

    LeetCode #133

--- DeepCopyUndirectedGraph/src/deepCopyUndirectedGraph/DeepCopyUndirectedGraph.java
+++ DeepCopyUndirectedGraph/src/deepCopyUndirectedGraph/DeepCopyUndirectedGraph.java
@@ -2,12 +2,17 @@ package deepCopyUndirectedGraph;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+
+// LeetCode #133 (Clone Graph).
 
 // Make a deep copy of an undirected graph, there could be cycles in the original graph.
 
 public class DeepCopyUndirectedGraph {
 
+	// DFS
 	public List<GraphNode> copy(List<GraphNode> graph) {
 		if (graph == null) {
 			return null;
@@ -33,6 +38,37 @@ public class DeepCopyUndirectedGraph {
 		}
 	}
 
-	// Time complexity is O(|V|+|E|),
+	// Time complexity is O(|V|+|E|).
+	// Space complexity is O(|V|+|E|), because of call stack.
+	
+	// BFS
+	public List<GraphNode> copy2(List<GraphNode> graph) {
+		if (graph == null) {
+			return null;
+		}
+		Queue<GraphNode> queue = new LinkedList<>();
+		HashMap<GraphNode, GraphNode> map = new HashMap<>();
+		for (GraphNode node : graph) {
+			if (!map.containsKey(node)) {
+				queue.offer(node);
+				map.put(node, new GraphNode(node.key));
+			}
+			while (!queue.isEmpty()) {
+				GraphNode cur = queue.poll();
+				GraphNode copy = map.get(cur);
+				for (GraphNode neighbor : cur.neighbors) {
+					if (!map.containsKey(neighbor)) {
+						queue.offer(neighbor);
+						map.put(neighbor, new GraphNode(neighbor.key));
+					}
+					copy.neighbors.add(map.get(neighbor));
+				}
+			}
+		}
+		return new ArrayList<GraphNode>(map.values());
+	}
+	
+	// Time complexity is O(|V|+|E|).
 	// Space complexity is O(|V|+|E|).
 }
+
