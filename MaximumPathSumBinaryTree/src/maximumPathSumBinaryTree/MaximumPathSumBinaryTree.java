9dc3141304730efd9d9dcb119f1a1ca16b6c87bd
Fan Feng <fanfeng2015@gmail.com>
2018-06-08 15:05:39 -0700

    LeetCode N/A

--- MaximumPathSumBinaryTree/src/maximumPathSumBinaryTree/MaximumPathSumBinaryTree.java
+++ MaximumPathSumBinaryTree/src/maximumPathSumBinaryTree/MaximumPathSumBinaryTree.java
@@ -1,50 +1,32 @@
 package maximumPathSumBinaryTree;
 
 // Given a binary tree in which each node contains an integer number. 
+
 // Find the maximum possible sum from one leaf node to another leaf node. 
-// If there is no such path available, return Integer.MIN_VALUE (Java).
+// If there is no such path available, return Integer.MIN_VALUE.
 
 public class MaximumPathSumBinaryTree {
 
+	private int max = Integer.MIN_VALUE;
+
 	public int maxPathSum(TreeNode root) {
-		int[] max = new int[] { Integer.MIN_VALUE };
-		helper(root, max);
-		return max[0];
+		helper(root);
+		return max;
 	}
 
-	// helper method returns the max path sum from a leaf node to root
-	private int helper(TreeNode root, int[] max) {
+	private int helper(TreeNode root) {
 		if (root == null) {
 			return 0;
 		}
-		int left = helper(root.left, max);
-		int right = helper(root.right, max);
+		int left = helper(root.left);
+		int right = helper(root.right);
 		if (root.left != null && root.right != null) {
-			max[0] = Math.max(max[0], left + right + root.key);
-			return Math.max(left, right) + root.key;
+			max = Math.max(max, root.key + left + right);
+			return root.key + Math.max(left, right);
 		}
-		// root.left == null || root.right == null
-		// no need to update max[0]
-		return root.left == null ? right + root.key : left + root.key;
+		return root.left == null ? root.key + right : root.key + left;
 	}
 
-	// How neat recursion is!
 	// Time complexity is O(n).
-	// Space complexity is O(n), because of call-stack, if the binary tree is
-	// highly unbalanced.
-
-	// Notice the reason why we use int[] max:
-	// Because Java is always passing by value, if primitive type is passed, a
-	// copy with that value will be used throughout the method, therefore the
-	// value of the original variable doesn't change.
-
-	public static void main(String[] agrs) {
-		MaximumPathSumBinaryTree maximumPathSumBinaryTree = new MaximumPathSumBinaryTree();
-		TreeNode root = new TreeNode(-15);
-		root.left = new TreeNode(2);
-		root.right = new TreeNode(11);
-		root.right.left = new TreeNode(6);
-		root.right.right = new TreeNode(14);
-		System.out.println(maximumPathSumBinaryTree.maxPathSum(root));
-	}
+	// Space complexity is O(n), because of call-stack, if the binary tree is highly unbalanced.
 }
