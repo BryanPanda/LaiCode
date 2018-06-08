1986eb05ba5a3bd029b36b53479c107965e25993
Fan Feng <fanfeng2015@gmail.com>
2018-06-08 15:29:53 -0700

    LeetCode N/A

--- MaximumPathSumBinaryTree4/src/maximumPathSumBinaryTree4/MaximumPathSumBinaryTree4.java
+++ MaximumPathSumBinaryTree4/src/maximumPathSumBinaryTree4/MaximumPathSumBinaryTree4.java
@@ -7,7 +7,6 @@ package maximumPathSumBinaryTree4;
 
 public class MaximumPathSumBinaryTree4 {
 
-	// solution 1
 	public int maxPathSum(TreeNode root) {
 		if (root.left == null && root.right == null) {
 			return root.key;
@@ -20,59 +19,4 @@ public class MaximumPathSumBinaryTree4 {
 
 	// Time complexity is O(n).
 	// Space complexity is O(n), if the binary tree is highly unbalanced.
-
-	// solution 2: bottom up return the max suffix sum
-	public int maxPathSum2(TreeNode root) {
-		if (root.left == null && root.right == null) {
-			return root.key;
-		}
-		if (root.left == null) {
-			return maxPathSum2(root.right) + root.key;
-		}
-		if (root.right == null) {
-			return maxPathSum2(root.left) + root.key;
-		}
-		return root.key + Math.max(maxPathSum2(root.left), maxPathSum2(root.right));
-	}
-
-	// Time complexity is O(n).
-	// Space complexity is O(n), if the binary tree is highly unbalanced.
-
-	// solution 3: top down pass the prefix sum
-	public int maxPathSum3(TreeNode root) {
-		return helper(root, 0);
-	}
-
-	public int helper(TreeNode root, int prefixSum) {
-		prefixSum += root.key;
-		if (root.left == null && root.right == null) {
-			return prefixSum;
-		} else if (root.left == null) {
-			return helper(root.right, prefixSum);
-		} else if (root.right == null) {
-			return helper(root.left, prefixSum);
-		}
-		return Math.max(helper(root.left, prefixSum), helper(root.right, prefixSum));
-	}
-
-	// Time complexity is O(n).
-	// Space complexity is O(n), if the binary tree is highly unbalanced.
-
-	public static void main(String[] args) {
-		MaximumPathSumBinaryTree4 maximumPathSumBinaryTree4 = new MaximumPathSumBinaryTree4();
-		TreeNode root = new TreeNode(3);
-		System.out.println(maximumPathSumBinaryTree4.maxPathSum(root));
-		root = new TreeNode(-1);
-		System.out.println(maximumPathSumBinaryTree4.maxPathSum(root));
-		root.left = new TreeNode(2);
-		System.out.println(maximumPathSumBinaryTree4.maxPathSum(root));
-		root.right = new TreeNode(4);
-		System.out.println(maximumPathSumBinaryTree4.maxPathSum(root));
-		root.left.left = new TreeNode(-5);
-		System.out.println(maximumPathSumBinaryTree4.maxPathSum(root));
-		root.left.left.right = new TreeNode(7);
-		System.out.println(maximumPathSumBinaryTree4.maxPathSum(root));
-		root.left.left.left = new TreeNode(10);
-		System.out.println(maximumPathSumBinaryTree4.maxPathSum(root));
-	}
 }
