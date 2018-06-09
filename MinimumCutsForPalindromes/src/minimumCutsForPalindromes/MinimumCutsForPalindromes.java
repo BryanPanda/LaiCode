718d2eacf20f65bf1745dc29bac0545030bb12f2
Fan Feng <fanfeng2015@gmail.com>
2018-06-08 17:48:25 -0700

    LeetCode N/A

--- MinimumCutsForPalindromes/src/minimumCutsForPalindromes/MinimumCutsForPalindromes.java
+++ MinimumCutsForPalindromes/src/minimumCutsForPalindromes/MinimumCutsForPalindromes.java
@@ -1,64 +1,24 @@
 package minimumCutsForPalindromes;
 
-import java.util.Arrays;
+// Given a string, a partitioning of the string is a palindrome partitioning if every
+// substring of the partition is a palindrome. 
 
-// Given a string, a partitioning of the string is a palindrome partitioning if 
-// every substring of the partition is a palindrome. 
 // Determine the fewest cuts needed for a palindrome partitioning of a given string.
 
-// Assumption: The given string is not null
+// Assumption: The given string is not null.
 
 public class MinimumCutsForPalindromes {
 
-	// solution 1
-	// minCuts[i]: min cuts needed for palindrome partitioning of substring from
-	// index 0 to i
+	// isPalindrome[i][j]: whether substring [i, j] is a palindrome
+	// minCuts[i]: min cuts needed to palindrome partition substring [0, i]
 	public int minCuts(String input) {
 		if (input.length() == 0) {
 			return 0;
 		}
-		int[] minCuts = new int[input.length()];
-		Arrays.fill(minCuts, Integer.MAX_VALUE);
-		minCuts[0] = 0;
-		for (int i = 1; i < input.length(); i++) {
-			if (isPalindrome(input, 0, i)) {
-				minCuts[i] = 0;
-			}
-			for (int j = 0; j < i; j++) {
-				if (isPalindrome(input, j + 1, i)) {
-					minCuts[i] = Math.min(minCuts[i], minCuts[j] + 1);
-				}
-			}
-		}
-		return minCuts[minCuts.length - 1];
-	}
-
-	private boolean isPalindrome(String input, int left, int right) {
-		while (left < right) {
-			if (input.charAt(left) != input.charAt(right)) {
-				return false;
-			}
-			left++;
-			right--;
-		}
-		return true;
-	}
-
-	// Time complexity is O(n^3).
-	// Space complexity is O(n).
-
-	// solution 2
-	// minCuts[i]: min cuts needed for palindrome partitioning of substring from
-	// index 0 to i, inclusive
-	// isPalindrome[i][j]: whether substring from index i to j, inclusive, is a
-	// palindrome
-	public int minCuts2(String input) {
-		if (input.length() == 0) {
-			return 0;
-		}
-		boolean[][] isPalindrome = new boolean[input.length()][input.length()];
-		int[] minCuts = new int[input.length()];
-		for (int i = 1; i < input.length(); i++) {
+		int n = input.length();
+		boolean[][] isPalindrome = new boolean[n][n];
+		int[] minCuts = new int[n];
+		for (int i = 1; i < n; i++) {
 			minCuts[i] = i;
 			for (int j = 0; j <= i; j++) {
 				if (input.charAt(j) == input.charAt(i)) {
@@ -69,14 +29,9 @@ public class MinimumCutsForPalindromes {
 				}
 			}
 		}
-		return minCuts[minCuts.length - 1];
+		return minCuts[n - 1];
 	}
 
 	// Time complexity is O(n^2).
 	// Space complexity is O(n^2).
-
-	public static void main(String[] args) {
-		MinimumCutsForPalindromes minimumCutsForPalindromes = new MinimumCutsForPalindromes();
-		System.out.println(minimumCutsForPalindromes.minCuts2("aaaaaabbabb"));
-	}
 }
