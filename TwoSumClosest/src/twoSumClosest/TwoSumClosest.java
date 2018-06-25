a855be1877295200f2115678d53b6882f4279f8e
Fan Feng <fanfeng2015@gmail.com>
2018-06-25 14:26:31 -0700

    LeetCode N/A

--- TwoSumClosest/src/twoSumClosest/TwoSumClosest.java
+++ TwoSumClosest/src/twoSumClosest/TwoSumClosest.java
@@ -3,39 +3,24 @@ package twoSumClosest;
 import java.util.Arrays;
 import java.util.List;
 
-// Find the pair of elements in a given array that sum to a value that is closest to the given target number. 
+// Find the pair of elements in a given array that sum to a value that is closest 
+// to the given target number.
+
 // Return the values of the two numbers.
 
 public class TwoSumClosest {
 
 	public List<Integer> closest(int[] array, int target) {
-		int minDiff = Integer.MAX_VALUE, one = 0, two = 0;
-		for (int i = 0; i < array.length; i++) {
-			for (int j = i + 1; j < array.length; j++) {
-				if (Math.abs(array[i] + array[j] - target) < minDiff) {
-					minDiff = Math.abs(array[i] + array[j] - target);
-					one = array[i];
-					two = array[j];
-				}
-			}
-		}
-		return Arrays.asList(one, two);
-	}
-
-	// Time complexity is O(n^2).
-	// Space complexity is O(1).
-
-	public List<Integer> closest2(int[] array, int target) {
+		int min = Integer.MAX_VALUE, one = 0, two = 0;
 		Arrays.sort(array);
-		int minDiff = Integer.MAX_VALUE, left = 0, right = array.length - 1;
-		int one = 0, two = 0;
+		int left = 0, right = array.length - 1;
 		while (left < right) {
-			if (Math.abs(array[left] + array[right] - target) < minDiff) {
-				minDiff = Math.abs(array[left] + array[right] - target);
+			if (Math.abs(array[left] + array[right] - target) < min) {
+				min = Math.abs(array[left] + array[right] - target);
 				one = array[left];
 				two = array[right];
 			}
-			if (array[left] + array[right] == target) {
+			if (min == 0) {
 				return Arrays.asList(one, two);
 			} else if (array[left] + array[right] < target) {
 				left++;
@@ -46,7 +31,6 @@ public class TwoSumClosest {
 		return Arrays.asList(one, two);
 	}
 
-	// Time complexity is O(n^2) in the worst case, because Arrays.sort() uses
-	// Quick Sort for primitive types.
-	// Space complexity is O(n) in the worst case, because of call-stack.
+	// Time complexity is O(n * log(n)) because of quick sort (for primitive types).
+	// Space complexity is O(log(n)).
 }
