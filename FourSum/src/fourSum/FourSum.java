49db55d573ef8b962f874bb2f60e5df70abbcac3
Fan Feng <fanfeng2015@gmail.com>
2018-06-26 15:27:21 -0700

    LeetCode N/A

--- FourSum/src/fourSum/FourSum.java
+++ FourSum/src/fourSum/FourSum.java
@@ -4,38 +4,23 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-// Determine if there exists a set of four elements in a given array that sum to the given target number.
+// Determine if there exists a set of four elements in a given array that sum to the 
+// given target number.
 
-// Assumption: The given array is not null and has length of at least 4
+// Assumption: The given array is not null and has length of at least 4.
+
+// Solutions:
+// 0. Sort + Three Sum: Time complexity is O(n^3), space complexity is O(n * log(n)), 
+//    because of quick sort (for primitive types).
+// 1. Construct all pairs + Two Sum (sort + two pointers): Time complexity is 
+//    O(n^2 * log(n^2)) = O(n^2 * log(n)), space complexity is O(n^2), because of merge
+//    sort (for non-primitive types).
+// 2. Construct all pairs + Two Sum (with hash map): Time complexity is O(n^2), space 
+//    complexity is O(n^2).
 
 public class FourSum {
 
-	// Solution 1: Sort + ThreeSum, O(n^3)
-	public boolean exist(int[] array, int target) {
-		Arrays.sort(array);
-		for (int i = 0; i < array.length - 3; i++) {
-			for (int j = i + 1; j < array.length - 2; j++) {
-				int left = j + 1, right = array.length - 1;
-				int curTarget = target - array[i] - array[j];
-				while (left < right) {
-					int sum = array[left] + array[right];
-					if (sum == curTarget) {
-						return true;
-					} else if (sum < curTarget) {
-						left++;
-					} else {
-						right--;
-					}
-				}
-			}
-		}
-		return false;
-	}
-
-	// Time complexity is O(n^3).
-	// Space complexity is O(1).
-
-	// Solution 2:contruct all possible pairs, and run TwoSum
+	// Solution 1
 	static class Element implements Comparable<Element> {
 		int left;
 		int right;
@@ -47,7 +32,7 @@ public class FourSum {
 			this.sum = sum;
 		}
 
-		// order of Element is first by sum, then by right, the by left
+		// Order Element first by sum, then by right, then by left.
 		@Override
 		public int compareTo(Element another) {
 			if (this.sum != another.sum) {
@@ -59,21 +44,14 @@ public class FourSum {
 			}
 			return 0;
 		}
-
-		@Override
-		public String toString() {
-			return "l: " + this.left + ", r: " + this.right + ", sum: " + this.sum;
-		}
 	}
 
-	public boolean exist2(int[] array, int target) {
-		Arrays.sort(array);
-		Element[] pairs = getAllPairs(array);
+	public boolean exist(int[] array, int target) {
+		Arrays.sort(array); // important (?)
+		Element[] pairs = constructAllPairs(array);
 		Arrays.sort(pairs);
 		int left = 0, right = pairs.length - 1;
-		// run TwoSum
 		while (left < right) {
-			// need to guarantee that no elements appears more than once
 			if (pairs[left].sum + pairs[right].sum == target && pairs[left].right < pairs[right].left) {
 				return true;
 			} else if (pairs[left].sum + pairs[right].sum < target) {
@@ -85,44 +63,33 @@ public class FourSum {
 		return false;
 	}
 
-	private Element[] getAllPairs(int[] array) {
+	private Element[] constructAllPairs(int[] array) {
 		Element[] pairs = new Element[array.length * (array.length - 1) / 2];
 		int cur = 0;
-		for (int i = 1; i < array.length; i++) {
-			for (int j = 0; j < i; j++) {
-				pairs[cur++] = new Element(j, i, array[j] + array[i]);
+		for (int i = 0; i < array.length; i++) {
+			for (int j = i + 1; j < array.length; j++) {
+				pairs[cur++] = new Element(i, j, array[i] + array[j]);
 			}
 		}
 		return pairs;
 	}
 
-	// Time complexity is O(n^2 * log(n^2)) = O(n^2 * log(n)), because
-	// Arrays.sort() uses Merge Sort for non-primitive types
+	// Time complexity is O(n^2 * log(n)).
 	// Space complexity is O(n^2).
 
-	static class Pair {
-		int left;
-		int right;
-
-		Pair(int left, int right) {
-			this.left = left;
-			this.right = right;
-		}
-	}
-
-	// Solution 3:
-	public boolean exist3(int[] array, int target) {
-		Map<Integer, Pair> map = new HashMap<>();
+	// Solution 2
+	public boolean exist2(int[] array, int target) {
+		Map<Integer, Element> map = new HashMap<>();
 		for (int i = 1; i < array.length; i++) {
 			for (int j = 0; j < i; j++) {
 				int sum = array[j] + array[i];
 				if (map.containsKey(target - sum) && map.get(target - sum).right < j) {
 					return true;
 				}
-				// otherwise, we need to insert, but for the same sum, only the
-				// pair with the smallest right index is needed
+				// If sum does not exist in map, put it into map, otherwise keep the current
+				// one since it has the smaller right index. (?)
 				if (!map.containsKey(sum)) {
-					map.put(sum, new Pair(j, i));
+					map.put(sum, new Element(j, i, sum));
 				}
 			}
 		}
