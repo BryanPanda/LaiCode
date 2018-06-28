9eb61318b2a0f7132943b5546cc3f080ae17972e
Fan Feng <fanfeng2015@gmail.com>
2018-06-28 00:27:41 -0700

    LeetCode N/A

--- KthSmallestWithFactorsThreeFiveSeven/src/kthSmallestWithFactorsThreeFiveSeven/KthSmallestWithFactorsThreeFiveSeven.java
+++ KthSmallestWithFactorsThreeFiveSeven/src/kthSmallestWithFactorsThreeFiveSeven/KthSmallestWithFactorsThreeFiveSeven.java
@@ -1,6 +1,5 @@
 package kthSmallestWithFactorsThreeFiveSeven;
 
-import java.util.Deque;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
@@ -13,7 +12,7 @@ import java.util.Set;
 
 public class KthSmallestWithFactorsThreeFiveSeven {
 
-	// Solution 1: BFS2
+	// Solution 1: BFS + heap (with hash set)
 	public long kth(int k) {
 		Set<Long> visited = new HashSet<>();
 		PriorityQueue<Long> minHeap = new PriorityQueue<>(k);
@@ -34,30 +33,25 @@ public class KthSmallestWithFactorsThreeFiveSeven {
 		return minHeap.peek();
 	}
 
-	// Notice: we could use a 3-D space to keep a record of x, y, z, but it is
-	// not necessary, and takes extra space.
-	// However, notice that all 3 factors are co-prime with each other,
-	// hence every product is unique, only under this condition can we use
-	// Set<Long>.
+	// Notice that all 3 factors are co-prime with each other, therefore every
+	// product is unique, only under this condition can we use hash set.
 
 	// What if the given factors are not co-prime with each other?
-	// 1. Use List<Integer>() to keep the number of each factor, hash code of
-	// List is already taken care of.
-	// 2. Use a static class, but need to override int hashcode() ourselves.
+	// Use Set<List<Integer>> to keep the number of each factor, hashCode() of List
+	// has already been implemented.
 
-	// Time complexity is O(k*log(k)).
+	// Time complexity is O(k * log(k)).
 	// Space complexity is O(k).
 
 	// Solution 2: linear in time, with 3 deques
 	public long kth2(int k) {
-		long base = 3 * 5 * 7L;
-		long result = base;
-		Deque<Long> three = new LinkedList<>();
-		Deque<Long> five = new LinkedList<>();
-		Deque<Long> seven = new LinkedList<>();
-		three.add(base * 3);
-		five.add(base * 5);
-		seven.add(base * 7);
+		long result = 3 * 5 * 7L;
+		LinkedList<Long> three = new LinkedList<>();
+		LinkedList<Long> five = new LinkedList<>();
+		LinkedList<Long> seven = new LinkedList<>();
+		three.add(result * 3);
+		five.add(result * 5);
+		seven.add(result * 7);
 		for (int i = 0; i < k - 1; i++) {
 			if (three.peekFirst() < five.peekFirst() && three.peekFirst() < seven.peekFirst()) {
 				result = three.pollFirst();
@@ -66,13 +60,12 @@ public class KthSmallestWithFactorsThreeFiveSeven {
 				seven.offerLast(result * 7);
 			} else if (five.peekFirst() < three.peekFirst() && five.peekFirst() < seven.peekFirst()) {
 				result = five.pollFirst();
-				// don't need to insert to Deque<Long> three, because result * 3
-				// has already been generated, in previous steps
+				// Don't need to insert to LinkedList<Long> three, because result * 3 has
+				// already been generated, in previous steps.
 				five.offerLast(result * 5);
 				seven.offerLast(result * 7);
 			} else {
 				result = seven.pollFirst();
-				// same here
 				seven.offerLast(result * 7);
 			}
 		}
