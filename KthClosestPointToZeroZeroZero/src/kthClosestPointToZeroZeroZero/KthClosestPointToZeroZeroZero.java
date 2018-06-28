7aea4dea25589e7736b7b4b276e2bf1af72e846f
Fan Feng <fanfeng2015@gmail.com>
2018-06-28 14:31:37 -0700

    LeetCode N/A

--- KthClosestPointToZeroZeroZero/src/kthClosestPointToZeroZeroZero/KthClosestPointToZeroZeroZero.java
+++ KthClosestPointToZeroZeroZero/src/kthClosestPointToZeroZeroZero/KthClosestPointToZeroZeroZero.java
@@ -8,58 +8,53 @@ import java.util.PriorityQueue;
 import java.util.Set;
 
 // Given three arrays sorted in ascending order. 
-// Pull one number from each array to form a coordinate <x,y,z> in a 3D space. 
-// Find the coordinates of the points that is k-th closest to <0,0,0>.
-// We are using euclidean distance here.
+
+// Pull one number from each array to form a coordinate <x, y, z> in a 3D space.
+
+// Find the coordinates of the points that is k-th closest to <0, 0, 0>. We are using euclidean distance
+// here.
 
 // Assumption:
-// 1. The three given arrays are not null or empty
-// 2. K >= 1 and K <= a.length * b.length * c.length
+// 1. The three given arrays are not null or empty.
+// 2. K >= 1 and K <= a.length * b.length * c.length.
 
 public class KthClosestPointToZeroZeroZero {
 
 	public List<Integer> closest(final int[] a, final int[] b, final int[] c, int k) {
-		// KthSmallestWithFactorsThreeFiveSeven asks for the k-th smallest
-		// number only, so PriorityQueue<Long> is enough.
-		// KthClosestPointToZeroZeroZero asks for the point with x, y, z
-		// coordinates.
+		// Integers in the list have to be indices in array a, b and c, otherwise
+		// duplicate list of values will not be inserted into the heap.
+		Set<List<Integer>> visited = new HashSet<>();
 		PriorityQueue<List<Integer>> minHeap = new PriorityQueue<>(k, new Comparator<List<Integer>>() {
 			@Override
-			public int compare(List<Integer> o1, List<Integer> o2) {
-				long d1 = distance(o1, a, b, c);
-				long d2 = distance(o2, a, b, c);
+			public int compare(List<Integer> p1, List<Integer> p2) {
+				long d1 = distance(p1, a, b, c);
+				long d2 = distance(p2, a, b, c);
 				if (d1 == d2) {
 					return 0;
 				}
 				return d1 < d2 ? -1 : 1;
 			}
 		});
-		Set<List<Integer>> visited = new HashSet<>();
-		// need to store indices in list, because a. this is what needs to be
-		// returned; b. list of indices is unique, but list of coordinates might
-		// not be
 		List<Integer> cur = Arrays.asList(0, 0, 0);
-		minHeap.offer(cur);
 		visited.add(cur);
+		minHeap.offer(cur);
 		for (int i = 0; i < k - 1; i++) {
 			cur = minHeap.poll();
 			List<Integer> next = Arrays.asList(cur.get(0) + 1, cur.get(1), cur.get(2));
-			if (next.get(0) < a.length && visited.add(next)) {
+			if (cur.get(0) + 1 < a.length && visited.add(next)) {
 				minHeap.offer(next);
 			}
 			next = Arrays.asList(cur.get(0), cur.get(1) + 1, cur.get(2));
-			if (next.get(1) < b.length && visited.add(next)) {
+			if (cur.get(1) + 1 < b.length && visited.add(next)) {
 				minHeap.offer(next);
 			}
 			next = Arrays.asList(cur.get(0), cur.get(1), cur.get(2) + 1);
-			if (next.get(2) < c.length && visited.add(next)) {
+			if (cur.get(2) + 1 < c.length && visited.add(next)) {
 				minHeap.offer(next);
 			}
 		}
-		cur.set(0, a[minHeap.peek().get(0)]);
-		cur.set(1, b[minHeap.peek().get(1)]);
-		cur.set(2, c[minHeap.peek().get(2)]);
-		return cur;
+		cur = minHeap.poll();
+		return Arrays.asList(new Integer[] { a[cur.get(0)], b[cur.get(1)], c[cur.get(2)] });
 	}
 
 	private Long distance(List<Integer> point, int[] a, int[] b, int[] c) {
@@ -72,5 +67,4 @@ public class KthClosestPointToZeroZeroZero {
 
 	// Time complexity is O(k).
 	// Space complexity is O(k).
-
 }
