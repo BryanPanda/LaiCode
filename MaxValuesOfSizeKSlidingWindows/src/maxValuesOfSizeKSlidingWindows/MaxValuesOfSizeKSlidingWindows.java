c20a83bd11101b68b6d7171ab91e7321a605da6f
Fan Feng <fanfeng2015@gmail.com>
2018-07-02 12:58:00 -0700

    LeetCode #239

--- MaxValuesOfSizeKSlidingWindows/src/maxValuesOfSizeKSlidingWindows/MaxValuesOfSizeKSlidingWindows.java
+++ MaxValuesOfSizeKSlidingWindows/src/maxValuesOfSizeKSlidingWindows/MaxValuesOfSizeKSlidingWindows.java
@@ -5,18 +5,20 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
+// LeetCode #239 (Sliding Window Maximum).
+
 // Given an integer array A and a sliding window of size K, 
 // find the maximum value of each window as it slides from left to right.
 
 // Assumptions:
-// 1. The given array is not null and is not empty
-// 2. K >= 1, K <= A.length
+// 1. The given array is not null and is not empty.
+// 2. K >= 1, K <= A.length.
 
 public class MaxValuesOfSizeKSlidingWindows {
 
 	public List<Integer> maxWindows(int[] array, int k) {
 		List<Integer> result = new ArrayList<Integer>();
-		// a descending deque with indices stored in it
+		// a descending deque, but stores indices instead of actual values
 		Deque<Integer> deque = new LinkedList<>();
 		for (int i = 0; i < array.length; i++) {
 			// discard indices with value smaller than array[i]
@@ -36,10 +38,4 @@ public class MaxValuesOfSizeKSlidingWindows {
 
 	// Time complexity is O(n).
 	// Space complexity is O(k).
-
-	public static void main(String[] agrs) {
-		MaxValuesOfSizeKSlidingWindows maxValuesOfSizeKSlidingWindows = new MaxValuesOfSizeKSlidingWindows();
-		int[] array = new int[] { 1, 2, 3, 2, 4, 2, 1 };
-		System.out.println(maxValuesOfSizeKSlidingWindows.maxWindows(array, 3).toString());
-	}
 }
